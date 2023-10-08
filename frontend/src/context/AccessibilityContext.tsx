718ce8fd7b579bfd0906384aa70a4e299318a1a9
khalidbelhadj <106029014+khalidbelhadj@users.noreply.github.com>
2023-10-08 12:43:26 +0100

    Refactor accessibility context

--- frontend/src/context/AccessibilityContext.tsx
+++ frontend/src/context/AccessibilityContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-type colorBlindness =
+type ColorBlindness =
   | "protanopia"
   | "deuteranopia"
   | "tritanopia"
@@ -9,22 +9,8 @@ type colorBlindness =
   | "none";
 
 type AccessibilityContextProps = {
-  useEyes: boolean;
-  setUseEyes: React.Dispatch<React.SetStateAction<boolean>>;
-  useFingers: boolean;
-  setUseFingers: React.Dispatch<React.SetStateAction<boolean>>;
-  useVoice: boolean;
-  setUseVoice: React.Dispatch<React.SetStateAction<boolean>>;
-  isHearingImpaired: boolean;
-  setHearingImpaired: React.Dispatch<React.SetStateAction<boolean>>;
-  soundAdjustment: number;
-  setSoundAdjustment: React.Dispatch<React.SetStateAction<number>>;
-  useDarkMode: boolean;
-  setUseDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-  useMagnifier: boolean;
-  setUseMagnifier: React.Dispatch<React.SetStateAction<boolean>>;
-  colorBlindness: colorBlindness;
-  setColorBlindness: React.Dispatch<React.SetStateAction<colorBlindness>>;
+  settings: Settings;
+  setSettings: (settings: Settings) => void;
 };
 
 const AccessibilityContext = createContext<
@@ -41,119 +27,41 @@ export const useAccessibilityContext = () => {
   return context;
 };
 
+const defaultSettings = {
+  useEyes: false,
+  useFingers: false,
+  useVoice: false,
+  isHearingImpaired: false,
+  soundAdjustment: 0,
+  useDarkMode: false,
+  useMagnifier: false,
+  colorBlindness: "none" as ColorBlindness,
+};
+
+type Settings = typeof defaultSettings;
+
+function getDefaultSettings(): Settings {
+  const storedSettings = localStorage.getItem("settings");
+
+  return storedSettings
+    ? (JSON.parse(storedSettings) as Settings)
+    : defaultSettings;
+}
+
 export const AccessibilityContextProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const [useEyes, setUseEyes] = useState<boolean>(() => {
-    // Load useEyes from localStorage, default to false if not found
-    const storedValue = localStorage.getItem("useEyes");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
-
-  const [useFingers, setUseFingers] = useState<boolean>(() => {
-    // Load useFingers from localStorage, default to false if not found
-    const storedValue = localStorage.getItem("useFingers");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
-
-  const [useVoice, setUseVoice] = useState<boolean>(() => {
-    // Load useVoice from localStorage, default to false if not found
-    const storedValue = localStorage.getItem("useVoice");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
-
-  const [isHearingImpaired, setHearingImpaired] = useState<boolean>(() => {
-    // Load isHearingImpaired from localStorage, default to false if not found
-    const storedValue = localStorage.getItem("isHearingImpaired");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
-
-  const [soundAdjustment, setSoundAdjustment] = useState<number>(() => {
-    // Load soundAdjustment from localStorage, default to 0 if not found
-    const storedValue = localStorage.getItem("soundAdjustment");
-    return storedValue ? JSON.parse(storedValue) : 0;
-  });
-
-  const [useDarkMode, setUseDarkMode] = useState<boolean>(() => {
-    // Load useDarkMode from localStorage, default to false if not found
-    const storedValue = localStorage.getItem("useDarkMode");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
-
-  const [useMagnifier, setUseMagnifier] = useState<boolean>(() => {
-    // Load useMagnifier from localStorage, default to false if not found
-    const storedValue = localStorage.getItem("useMagnifier");
-    return storedValue ? JSON.parse(storedValue) : false;
-  });
-
-  const [colorBlindness, setColorBlindness] = useState<colorBlindness>(() => {
-    // Load colorBlindness from localStorage, default to 'none' if not found
-    const storedValue = localStorage.getItem("colorBlindness");
-    return storedValue ? (storedValue as colorBlindness) : "none";
-  });
+  const [settings, setSettings] = useState<Settings>(getDefaultSettings);
 
   useEffect(() => {
-    // Save useEyes to localStorage whenever it changes
-    localStorage.setItem("useEyes", JSON.stringify(useEyes));
-  }, [useEyes]);
-
-  useEffect(() => {
-    // Save useFingers to localStorage whenever it changes
-    localStorage.setItem("useFingers", JSON.stringify(useFingers));
-  }, [useFingers]);
-
-  useEffect(() => {
-    // Save useVoice to localStorage whenever it changes
-    localStorage.setItem("useVoice", JSON.stringify(useVoice));
-  }, [useVoice]);
-
-  useEffect(() => {
-    // Save isHearingImpaired to localStorage whenever it changes
-    localStorage.setItem(
-      "isHearingImpaired",
-      JSON.stringify(isHearingImpaired),
-    );
-  }, [isHearingImpaired]);
-
-  useEffect(() => {
-    // Save soundAdjustment to localStorage whenever it changes
-    localStorage.setItem("soundAdjustment", JSON.stringify(soundAdjustment));
-  }, [soundAdjustment]);
-
-  useEffect(() => {
-    // Save useDarkMode to localStorage whenever it changes
-    localStorage.setItem("useDarkMode", JSON.stringify(useDarkMode));
-  }, [useDarkMode]);
-
-  useEffect(() => {
-    // Save useMagnifier to localStorage whenever it changes
-    localStorage.setItem("useMagnifier", JSON.stringify(useMagnifier));
-  }, [useMagnifier]);
-
-  useEffect(() => {
-    // Save colorBlindness to localStorage whenever it changes
-    localStorage.setItem("colorBlindness", colorBlindness);
-  }, [colorBlindness]);
+    localStorage.setItem("settings", JSON.stringify(settings));
+  });
 
   return (
     <AccessibilityContext.Provider
       value={{
-        useEyes,
-        setUseEyes,
-        useFingers,
-        setUseFingers,
-        useVoice,
-        setUseVoice,
-        isHearingImpaired,
-        setHearingImpaired,
-        soundAdjustment,
-        setSoundAdjustment,
-        useDarkMode,
-        setUseDarkMode,
-        useMagnifier,
-        setUseMagnifier,
-        colorBlindness,
-        setColorBlindness,
+        settings,
+        setSettings,
       }}
     >
       {children}
